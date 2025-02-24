34b0b24ed17e4d3cf8784c4ebe2a4f877317e071
hls <hilliassmirali@gmail.com>
2025-02-24 14:04:00 +0100

    Update the code of class bay adding parentheses after super function

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,16 +1,21 @@
-#!/usr/bin/ython3
+#!/usr/bin/python3
 """
 This file provide an user class
 """
-Base_class = __import__("base_class").Base_class
+Base_class = __import__("base_class").Baseclass
 
 class User(Base_class):
     """
     This class represent an user
     """
     def __init__(self, first_name, last_name, email, is_admin):
-        super.__init__()
+        super().__init__()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
+
+
+user1 = User("Hilliass", "Soumahill", "Hilliass@mail.com", False)
+
+print(user1.created_at)
