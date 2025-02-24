14e3c2fe2de7a55fc756775bfdce6daba9077f84
hls <hilliassmirali@gmail.com>
2025-02-24 13:59:34 +0100

    Create the classe user

---
+++ part2/app/models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/ython3
+"""
+This file provide an user class
+"""
+Base_class = __import__("base_class").Base_class
+
+class User(Base_class):
+    """
+    This class represent an user
+    """
+    def __init__(self, first_name, last_name, email, is_admin):
+        super.__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
