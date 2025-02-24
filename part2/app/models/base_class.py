37b1ef086ccca50ad266d8e56dcf8317a197e629
hls <hilliassmirali@gmail.com>
2025-02-24 13:51:26 +0100

    Update the base class

--- part2/app/models/base_class.py
+++ part2/app/models/base_class.py
@@ -11,3 +11,14 @@ class Baseclass:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+    def save(self):
+        """Update the updated_at timestamp whenever the object is modified"""
+        self.updated_at = datetime.now()
+    
+    def update(self, data):
+        """Update the attributes of the object based on the provided dictionary"""
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.save()
