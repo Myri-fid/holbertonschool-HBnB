9121970e9dfe3592b48d3aadde67d0cb370f3d3f
hls <hilliassmirali@gmail.com>
2025-02-25 13:19:44 +0100

    add setter and getter

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -12,19 +12,51 @@ class User(Base_class):
     """
     def __init__(self, first_name, last_name, email, is_admin):
         super().__init__()
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.is_admin = is_admin
-        self.place = []
+        self._first_namefirst_name = first_name
+        self._last_name = last_name
+        self._email = email
+        self._is_admin = is_admin
+        self._place = []
 
     @property
-    def get_first_name(self):
+    def first_name(self):
         return self._first_name
 
     @first_name.setter
     def first_name(self, value):
-        if re.match(r"^[A-Z][a-z]+$", value, re.UNICODE) is not None:
+        if re.match(r"^[A-Za-z][a-zA-Z]{2,49}+$", value, re.UNICODE) \
+              is not None:
             self._first_name = value
         else:
             raise ValueError("Wrong input !")
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, value):
+        if re.match(r"^[A-Za-z][a-zA-Z]{2,49}+$", value, re.UNICODE):
+            self._first_name = value
+        else:
+            raise ValueError("Wrong input !")
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        pattern = r"^[a-zA-Z0-9_.-]+@[a-zA-Z-_]+\.[a-zA-Z]{2,}$"
+        if re.match(pattern, value) is not None:
+            self._email = value
+        else:
+            raise ValueError("The format is wrong !")
+
+    @property
+    def place(self):
+        return self._place
+
+    @place.setter
+    def add_place(self, value):
+        self._place.append(value)
