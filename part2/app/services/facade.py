15d89294936b2891161e13a2a47057201777fa75
Myriam <myriiamwayne@gmail.com>
2025-02-28 18:26:09 +0100

    Refactor HBnBFacade by removing commented-out place methods and cleaning up imports

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,74 +1,18 @@
 from app.models.user import User
-<<<<<<< HEAD
-from app.persistence.repository import InMemoryRepository
-from app.models.place import Place
-=======
 from app.models.review import Review
 # from app.models.place import Place
 from app.models.amenity import Amenity
->>>>>>> origin/main
 from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-<<<<<<< HEAD
-        self.place_repository = InMemoryRepository()
-
-=======
 
     # User methods
->>>>>>> origin/main
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
         return user
-<<<<<<< HEAD
-
-    def get_user(self, user_id):
-        return self.user_repo.get(user_id)
-
-    def get_user_by_email(self, email):
-        return self.user_repo.get_by_attribute('email', email)
-
-    def create_place(self, place_data):
-    # Placeholder for logic to create a place, including validation for price, latitude, and longitude
-        try:
-            place = Place(
-                title=place_data['title'],
-                description=place_data.get('description', ''),
-                price=place_data['price'],
-                latitude=place_data['latitude'],
-                longitude=place_data['longitude'],
-                owner_id=place_data['owner_id'],
-                amenities=place_data['amenities']
-            )
-            self.place_repository.save(place)
-            return place
-        except Exception as e:
-            raise ValueError(f"Error creation place: {e}")
-         
-
-    def get_place(self, place_id):
-    # Placeholder for logic to retrieve a place by ID, including associated owner and amenities
-        return self.place_repository.get_all()
-
-    def get_all_places(self):
-    # Placeholder for logic to retrieve all places
-        return self.place_repository.get_all()
-
-    def update_place(self, place_id, place_data):
-    # Placeholder for logic to update a place
-        place = self.place_repository.get_by_id(place_id)
-        if not place:
-            raise ValueError(f"Place avec ID {place_id} pas trouvé")
-
-    def get_place_by_id(self, place_id):
-        place = self.place_repository.get_by_id(place_id)
-        if not place:
-            raise ValueError(f"Place avec ID {place_id} pas trouvé")
-        return place
-=======
 
     def get_user(self, user_id):
         return self.user_repo.get(user_id)
@@ -129,4 +73,3 @@ class HBnBFacade:
         amenity = self.amenity_repo.get(amenity_id)
         self.amenity_repo.delete(amenity)
         return amenity
->>>>>>> origin/main
