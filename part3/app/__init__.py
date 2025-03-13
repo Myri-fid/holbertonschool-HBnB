5f37f106abb2bae1337592831b68c45e6fbc4cf6
Myriam <myriiamwayne@gmail.com>
2025-03-13 14:16:41 +0100

    feat: integrate Flask-Bcrypt for password hashing and add protected endpoint

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,22 +1,26 @@
 from flask import Flask
 from flask_restx import Api
-from config import Config
+from config import DevelopmentConfig
 from flask_jwt_extended import JWTManager
 from app.api.v1.users import api as users_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.amenities import api as amenities_ns
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 # Initialisation de JWT
 jwt = JWTManager()
 
-def create_app(config_class=Config):
+def create_app(config_class=DevelopmentConfig):
     # Créer une instance de l'application Flask
     app = Flask(__name__)
     app.config.from_object(config_class)  # Charger la configuration
 
-    # Initialiser JWTManager
+    # Initialiser JWTManager et Flask-Bcrypt
     jwt.init_app(app)
+    bcrypt.init_app(app)
 
     # Initialiser l'API Flask-RESTx
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
@@ -26,4 +30,5 @@ def create_app(config_class=Config):
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+
     return app
