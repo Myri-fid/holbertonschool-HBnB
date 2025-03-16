94d278f1bd89e20a57b82c8c97233c2920bb8ce5
hls <hilliassmirali@gmail.com>
2025-03-16 23:24:25 +0100

    reorganize imports and enhance app initialization structure

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -3,11 +3,6 @@ from flask_restx import Api
 from config import DevelopmentConfig
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
-from app.api.v1.users import api as users_ns
-from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.amenities import api as amenities_ns
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 
@@ -20,6 +15,11 @@ def create_app(config_class=DevelopmentConfig):
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.amenities import api as amenities_ns
+
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(places_ns, path='/api/v1/places')
@@ -30,6 +30,11 @@ def create_app(config_class=DevelopmentConfig):
     bcrypt.init_app(app)
 
     with app.app_context():
+        from app.models.user import User
+        from app.models.amenity import Amenity
+        from app.models.place import Place
+        from app.models.review import Review
+
         db.create_all()
 
     return app
