76bcdfe1112c0e15742ab14955bc627fc884d66b
hls <hilliassmirali@gmail.com>
2025-03-16 21:19:34 +0100

    add SQLAlchemy integration and initialize database in app creation

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from config import DevelopmentConfig
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 from app.api.v1.users import api as users_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.places import api as places_ns
@@ -9,7 +10,7 @@ from app.api.v1.amenities import api as amenities_ns
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
-
+db = SQLAlchemy()
 jwt = JWTManager()
 
 def create_app(config_class=DevelopmentConfig):
@@ -18,6 +19,7 @@ def create_app(config_class=DevelopmentConfig):
 
     jwt.init_app(app)
     bcrypt.init_app(app)
+    db.init_app(app)
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
@@ -25,5 +27,8 @@ def create_app(config_class=DevelopmentConfig):
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+
+    with app.app_context():
+        db.create_all()
 
     return app
