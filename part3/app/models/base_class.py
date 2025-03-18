1e67bde67c8639a4f11828db4e82488644d37c72
hls <hilliassmirali@gmail.com>
2025-03-18 01:09:41 +0100

    improve formatting and organization in base_class.py

--- part3/app/models/base_class.py
+++ part3/app/models/base_class.py
@@ -7,12 +7,17 @@ import uuid
 from datetime import datetime
 from app import db
 
+
 class Baseclass(db.Model):
     __abstract__ = True
 
-    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    id = db.Column(db.String(36),
+                   primary_key=True,
+                   default=lambda: str(uuid.uuid4()))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    updated_at = db.Column(db.DateTime,
+                           default=datetime.utcnow,
+                           onupdate=datetime.utcnow)
 
     def save(self):
         self.updated_at = datetime.utcnow()
