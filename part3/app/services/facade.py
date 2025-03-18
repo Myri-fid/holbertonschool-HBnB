c0c30b8833c55971af482b174eedd0b8d4596bd0
hls <hilliassmirali@gmail.com>
2025-03-18 03:09:17 +0100

    remove unnecessary blank line in HBnBFacade class

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -29,7 +29,6 @@ class HBnBFacade:
     def get_all_users(self):
         return self.user_repo.get_all()
 
-
     def update_user(self, user_id, user_data):
         self.user_repo.update(user_id, user_data)
         return self.user_repo.get(user_id)
